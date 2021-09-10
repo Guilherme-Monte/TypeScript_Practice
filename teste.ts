6aac006866a38b4c3e1e4a6684417103e5eb4be5
Guilherme-Monte <guilhermemonte.dev@gmail.com>
2021-09-10 17:35:48 -0300

    Get started do typescriptlang.org

--- teste.ts
+++ teste.ts
@@ -1,2 +1,31 @@
-let helloWorld = "Hello World";
 let helloWorldTS = "Hello World TS";
+
+interface User {
+  name: String;
+  id: Number;
+};
+
+const user: User = {
+  name: "Hayes",
+  id: 1,
+};
+
+const user2: User = {
+  userName: "Hayes",
+  userId: 1,
+};
+
+// OOP:
+
+class UserAccount {
+  name: String;
+  id: Number;
+
+  constructor(name: String, id: Number) {
+    this.name = name;
+    this.id = id;
+  };
+}
+
+const user3: User = new UserAccount("Murphy", 1);
+const user4: User = new UserAccount("Error", "1");
